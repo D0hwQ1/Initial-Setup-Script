0e50c7f3c834d9947e1e561c30f68c415ef98389
D0hwQ1 <pjt137@naver.com>
2021-10-06 14:14:42 +0900

    Add: winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,16 @@
+winget install chrome
+winget install notion
+winget install --force kakaotalk
+winget install Parsec.Parsec
+winget install discord
+winget install telegram
+winget install slack
+winget install voidtools.Everything
+winget install potplayer
+winget install Microsoft.VisualStudioCode.Insiders
+winget install "Android Studio"
+winget install "python 2"
+winget install "python 3"
+winget install "Windows Terminal"
+winget install --force AdoptOpenJDK.OpenJDK.14
+winget install Ubuntu -v 18.04
