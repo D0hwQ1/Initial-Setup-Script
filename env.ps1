6457c2fd2cdb7802481fe20e6c05c6d3d5ab7403
Park Jongtae <64513089+D0hwQ1@users.noreply.github.com>
2021-10-06 14:18:47 +0900

    Update: env.ps1

--- env.ps1
+++ env.ps1
@@ -1,6 +1,13 @@
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Python27", "Machine")
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Users\pjt137\AppData\Local\Programs\Python\Python39\Scripts", "Machine")
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Users\pjt137\AppData\Local\Programs\Python\Python39", "Machine")
+Function env([string]$env) {
+    $INCLUDE = $env
+    $OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','machine')
+    $NEWPATH = "$OLDPATH;$INCLUDE"
+    [Environment]::SetEnvironmentVariable("PATH", "$NEWPATH", "Machine")
+}
+
+env("C:\Python27")
+env("C:\Users\pjt137\AppData\Local\Programs\Python\Python39\Scripts")
+env("C:\Users\pjt137\AppData\Local\Programs\Python\Python39")
 
 net localgroup administrators $(whoami) /add
 
