a1678046053213745a59e21b877b48a2c1cf51bc
D0hwQ1 <pjt137@naver.com>
2021-11-02 18:25:16 +0900

    Update: winget.ps1

--- src/winget.ps1
+++ src/winget.ps1
@@ -1,15 +1,16 @@
-winget install --force chrome
+winget install --force chrome --source winget
 winget install notion
 winget install --force kakaotalk
-winget install --force parsec
-winget install discord
-winget install telegram
-winget install slack
+winget install --force parsec --source winget
+winget install --force discord --source winget
 winget install voidtools.Everything
 winget install wox
-winget install --force potplayer
+winget install --force potplayer --source winget
 winget install Microsoft.VisualStudioCode.Insiders
+winget install wireshark
 winget install "Android Studio"
 winget install "python 2"
+winget install python3
 winget install --force AdoptOpenJDK.OpenJDK.14
-winget install Ubuntu -v 18.04
+winget install Ubuntu -v 18.04 --source winget
+winget install terminal --source winget
